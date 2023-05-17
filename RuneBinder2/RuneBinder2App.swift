0c8d649553af782351447b3bbf2278f94693863f
RickySchober <rschober@scu.edu>
2023-05-16 20:03:51 -0700

    Initial Commit

---
+++ RuneBinder2/RuneBinder2App.swift
@@ -0,0 +1,17 @@
+//
+//  RuneBinder2App.swift
+//  RuneBinder2
+//
+//  Created by Ricky Schober on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct RuneBinder2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
